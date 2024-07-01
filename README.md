b87f16ec167b5250807ccd2c9b3263ef8070bbc2
Tasir Rahman <tasirrahman9@gmail.com>
2024-07-01 13:59:57 +0600

    starting point - 07/01/2024 - 1:59PM

---
+++ README.md
@@ -0,0 +1,3 @@
+# rest_api_flutter
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/rest_api_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.rest_api_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
